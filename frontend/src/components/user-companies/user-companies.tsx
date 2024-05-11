654a50b79f5de3591fbcc69551c4c5301519d335
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-05-11 23:21:38 +0600

    made user companies section responsive

--- frontend/src/components/user-companies/user-companies.tsx
+++ frontend/src/components/user-companies/user-companies.tsx
@@ -43,19 +43,19 @@ const UserCompanies = () => {
         <SecondHeading>
           200+ The fastest growing company use Finzo
         </SecondHeading>
-        <Paragraph className="mb-9 max-w-[574px] text-center">
+        <Paragraph className="mb-9 max-w-sm text-center lg:max-w-md">
           Many companies have tried using Finzo and they trust the safety of
           their money
         </Paragraph>
       </div>
-      <div className="grid grid-cols-4 gap-8">
+      <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 lg:gap-8">
         {companies.map((company) => {
           return (
             <div
               key={company.company}
-              className="flex items-center justify-center rounded-3xl bg-offwhite px-16 py-12"
+              className="flex items-center justify-center rounded-2xl bg-offwhite px-2 py-6 sm:px-4 sm:py-8 md:rounded-3xl lg:py-12"
             >
-              <div className="relative h-9 w-full">
+              <div className="relative h-6 w-full sm:h-7 lg:h-9">
                 <Image
                   src={company.loc}
                   alt={`Logo of ${company.company}`}
