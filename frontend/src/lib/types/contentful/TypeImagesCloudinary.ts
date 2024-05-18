7ab098a6bb91e81852aec60605190566e59dce22
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-05-19 00:42:55 +0600

    removed previous contetful packages and files

--- frontend/src/lib/types/contentful/TypeImagesCloudinary.ts
+++
@@ -1,8 +0,0 @@
-import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
-
-export interface TypeImagesCloudinaryFields {
-    cloudinaryObject: EntryFieldTypes.Object;
-}
-
-export type TypeImagesCloudinarySkeleton = EntrySkeletonType<TypeImagesCloudinaryFields, "imagesCloudinary">;
-export type TypeImagesCloudinary<Modifiers extends ChainModifiers, Locales extends LocaleCode = LocaleCode> = Entry<TypeImagesCloudinarySkeleton, Modifiers, Locales>;
--- frontend/src/lib/types/contentful/index.ts
+++
@@ -1,5 +0,0 @@
-export type { TypeImagesCloudinary, TypeImagesCloudinaryFields, TypeImagesCloudinarySkeleton } from "./TypeImagesCloudinary";
-export type { TypeNewsletterSubscribedEmails, TypeNewsletterSubscribedEmailsFields, TypeNewsletterSubscribedEmailsSkeleton } from "./TypeNewsletterSubscribedEmails";
-export type { TypeTestimonial, TypeTestimonialFields, TypeTestimonialSkeleton } from "./TypeTestimonial";
-export type { TypeTotalUserCompanyCount, TypeTotalUserCompanyCountFields, TypeTotalUserCompanyCountSkeleton } from "./TypeTotalUserCompanyCount";
-export type { TypeTotalUsersInMillions, TypeTotalUsersInMillionsFields, TypeTotalUsersInMillionsSkeleton } from "./TypeTotalUsersInMillions";
