fdaf4bcc6e23ac0e031606eeac3c392102c15ea3
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-05-18 14:27:41 +0600

    Implemented all cms operations using contentful

--- frontend/src/lib/actions.ts
+++ frontend/src/lib/actions.ts
@@ -1,18 +1,42 @@
 "use server";
 
-import { strapiClient } from "./strapi-client";
+import {
+  contentfulClient,
+  contentfulManagementClient,
+} from "./contentful/client";
+import { TypeNewsletterSubscribedEmailsSkeleton } from "./types/contentful";
 import { newsletterSubscribeSchema } from "./validation/news-letter";
 
 export const newsletterSubscribeAction = async (data: { email: string }) => {
   const parsed = newsletterSubscribeSchema.safeParse(data);
   if (!parsed.success) return { error: "Invalid email!" };
-  const res = await strapiClient.POST("/newsletter-subscribed-emails", {
-    body: { data: { email: data.email } },
-  });
-  if (res.error?.error.message === "This attribute must be unique") {
-    return { error: "This email is already subscribed!" };
-  }
-  if (res.error) {
+  try {
+    const alreadyExistingEmail =
+      (await contentfulClient
+        .getEntries<TypeNewsletterSubscribedEmailsSkeleton>({
+          content_type: "newsletterSubscribedEmails",
+          limit: 1,
+          "fields.email": parsed.data.email,
+        })
+        .then((res) => res.total)) > 0;
+
+    if (alreadyExistingEmail) {
+      return { error: "This email is already subscribed!" };
+    }
+
+    const space = await contentfulManagementClient.getSpace(
+      process.env.CONTENTFUL_SPACE_ID!,
+    );
+    const environment = await space.getEnvironment(
+      process.env.CONTENTFUL_ENVIRONMENT!,
+    );
+
+    await environment
+      .createEntry("newsletterSubscribedEmails", {
+        fields: { email: { "en-US": parsed.data.email } },
+      })
+      .then((res) => res.publish());
+  } catch (err) {
     return { error: `Something went wrong! Please try again.` };
   }
   return { success: "Subscribed to our newsletter successfully!" };
--- frontend/src/lib/server.ts
+++ frontend/src/lib/server.ts
@@ -2,27 +2,31 @@ import { cache } from "react";
 import "server-only";
 import { z } from "zod";
 import { contentfulClient } from "./contentful/client";
-import { strapiClient } from "./strapi-client";
-import { TypeTestimonialSkeleton } from "./types/contentful";
+import {
+  TypeTestimonialSkeleton,
+  TypeTotalUserCompanyCountSkeleton,
+  TypeTotalUsersInMillionsSkeleton,
+} from "./types/contentful";
 import { TestimonialsSchema } from "./validation/testimonial";
 
 export const getTestimonials = async () => {
-  const data = await contentfulClient.withoutUnresolvableLinks.getEntries<TypeTestimonialSkeleton>({
-    content_type: "testimonial",
-  });
+  const data =
+    await contentfulClient.withoutUnresolvableLinks.getEntries<TypeTestimonialSkeleton>(
+      {
+        content_type: "testimonial",
+      },
+    );
   const fetchedData = data.items.map((obj) => {
-    const imageField = obj.fields.image?.fields
+    const imageField = obj.fields.image?.fields;
     return {
       ...obj.fields,
       image: {
         url: "https:" + imageField?.file?.url,
         alt: imageField?.description,
-        width: imageField?.file?.details
-          .image?.width,
-        height: imageField?.file?.details
-          .image?.height,
+        width: imageField?.file?.details.image?.width,
+        height: imageField?.file?.details.image?.height,
       },
-    }
+    };
   });
   const parseData = TestimonialsSchema.safeParse(fetchedData);
   if (!parseData.success) return;
@@ -30,19 +34,27 @@ export const getTestimonials = async () => {
 };
 
 export const getTotalUsersInMillions = cache(async () => {
-  const fetchedData = await strapiClient.GET("/total-user-in-millions");
+  const fetchedData =
+    await contentfulClient.getEntries<TypeTotalUsersInMillionsSkeleton>({
+      content_type: "totalUsersInMillions",
+      limit: 1,
+    });
   const parsedData = z
     .number()
-    .safeParse(fetchedData.data?.data?.attributes?.count);
+    .safeParse(fetchedData.items.at(0)?.fields.value);
   if (!parsedData.success) return;
   return parsedData.data;
 });
 
 export const getTotalUserCompanies = cache(async () => {
-  const fetchedData = await strapiClient.GET("/total-user-company-count");
+  const fetchedData =
+    await contentfulClient.getEntries<TypeTotalUserCompanyCountSkeleton>({
+      content_type: "totalUserCompanyCount",
+      limit: 1,
+    });
   const parsedData = z
     .number()
-    .safeParse(fetchedData.data?.data?.attributes?.count);
+    .safeParse(fetchedData.items.at(0)?.fields.value);
   if (!parsedData.success) return;
   return parsedData.data;
 });
