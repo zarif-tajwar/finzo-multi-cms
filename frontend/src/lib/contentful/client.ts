c40d340e68d4ee893f3ff6816154e4abde8f58f2
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-05-18 12:58:26 +0600

    Contentful Init

---
+++ frontend/src/lib/contentful/client.ts
@@ -0,0 +1,7 @@
+import "server-only";
+import { createClient } from "contentful";
+
+export const contentfulClient = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+});
