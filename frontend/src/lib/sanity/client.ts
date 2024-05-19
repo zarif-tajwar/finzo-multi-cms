21feb256a1175fa95019da3656e742e388f6b0ae
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-05-19 12:15:58 +0600

    removed sanity client cache

--- frontend/src/lib/sanity/client.ts
+++ frontend/src/lib/sanity/client.ts
@@ -5,7 +5,7 @@ import { createClient, type QueryParams } from "next-sanity";
 export const sanityClient = createClient({
   projectId: "kmb63xxa",
   dataset: "production",
-  apiVersion: "2022-03-07",
+  apiVersion: "2024-01-01",
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
 });
@@ -21,8 +21,9 @@ export async function sanityFetch<QueryResponse>({
 }) {
   return sanityClient.fetch<QueryResponse>(query, params, {
     next: {
-      revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
       tags,
     },
+    cache: "no-store",
+    useCdn: false,
   });
 }
