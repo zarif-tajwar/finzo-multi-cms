f571aab003efec0844e3faf8af3587ac78a5539e
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2025-02-03 22:05:06 +0600

    Revert "feat: add posthog analytics"
    This reverts commit c679cc68503b5136ae58c9e094c3461a59371f69.

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -3,7 +3,6 @@ import { primaryFont } from "./_fonts/primary";
 import Navbar from "@/components/navbar/navbar";
 import { ReactLenis } from "@/lib/lenis";
 import { Toaster } from "@/components/ui/sonner";
-import PostHogProvider from "@/components/posthog-provider";
 
 export default function RootLayout({
   children,
@@ -13,13 +12,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${primaryFont.variable} font-primary`}>
-        <PostHogProvider>
-          <ReactLenis root options={{ lerp: 0.085 }}>
-            <Navbar />
-            {children}
-            <Toaster />
-          </ReactLenis>
-        </PostHogProvider>
+        <ReactLenis root options={{ lerp: 0.085 }}>
+          <Navbar />
+          {children}
+          <Toaster />
+        </ReactLenis>
       </body>
     </html>
   );
